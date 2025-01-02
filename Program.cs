f603128de504c4ce13666cd66672f0b020911e10
eirikkar <eirikkar@proton.me>
2025-01-02 22:15:36 +0100

    add printtitle method and tested the method

--- Program.cs
+++ Program.cs
@@ -11,5 +11,21 @@ class Program
             "Test text 2",
             "Test text 3",
         };
+        PrintTitle(
+            "Test text in this title hello world, how are you doing today",
+            ConsoleColor.Cyan
+        );
+    }
+
+    static void PrintTitle(string title, ConsoleColor color)
+    {
+        Console.ForegroundColor = color;
+        foreach (char c in title)
+        {
+            Console.Write(c);
+            Thread.Sleep(50); // Slow down printing for animation effect
+        }
+        Console.WriteLine();
+        Console.ResetColor();
     }
 }
